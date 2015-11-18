1aa1cbc2c75d9dac5ab77908801268352d1994a0
Henning Kage <henning.kage@gmail.com>
2015-11-18 15:52:36 +0100

    Added pytest environment

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
+import os.path
 from setuptools import setup, find_packages
 
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+exec(open('version.py').read())
+
 setup(name='tornado-project-skeleton',
-      version='0.1',
-      description='Tornado project skeleton',
-      long_description='',
-      author='Henning Kage',
-      author_email='henning.kage@gmail.com',
-      url='https://github.com/hkage/tornado-project-skeleton',
-      include_package_data=True,
-      classifiers=[],
-      packages=find_packages(exclude=['tests']),
-      install_requires=[
-          'tornado==4.2'
-      ],
-      tests_require=[
-          'pytest>=2.6.0',
-          'pytest-pep8',
-          'pytest-cov',
-          'tox'
-      ])
+    version=__version__,
+    description='Tornado project skeleton',
+    long_description=read('README.rst'),
+    author='Henning Kage',
+    author_email='henning.kage@gmail.com',
+    url='https://github.com/hkage/tornado-project-skeleton',
+    include_package_data=True,
+    classifiers=[],
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'tornado==4.3'
+    ],
+    setup_requires=[
+        'pytest-runner==2.6.2',
+    ],
+    tests_require=[
+        'pytest==2.8.2',
+        'pytest-pep8==1.0.6',
+        'pytest-cov==2.2.0',
+    ])
