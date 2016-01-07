71ea33f222f2d06f7e8e000475ca3c1b640d12f5
Henning Kage <henning.kage@gmail.com>
2016-01-07 08:53:20 +0100

    Fixed run script

--- run.py
+++ run.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#import os.path
-#import wsgiref.handlers
-
-#import tornado.auth
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.autoreload
-#from tornado.options import define, options
 from tornado.options import options
 import tornado.web
-#import tornado.wsgi
 
 from settings import settings
 from urls import url_patterns
 
 
 class TornadoApplication(tornado.web.Application):
+
     def __init__(self):
         tornado.web.Application.__init__(self, url_patterns, **settings)
 
 
 def main():
     app = TornadoApplication()
-    http_server = tornado.httpserver.HTTPServer(app)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == "__main__":
