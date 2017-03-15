3955f30dddb1d22a8668a7e8356b36abcb6b286c
Henning Kage <henning.kage@gmail.com>
2017-03-15 12:39:57 +0100

    Added copyright year to the Sphinx conf file

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'{{ cookiecutter.project_name }}'
-copyright = u'2016, {{ cookiecutter.author_name }}'
+copyright = u'{% now 'local', '%Y' %}, {{ cookiecutter.author_name }}'
 author = u'{{ cookiecutter.author_name }}'
 
 # The version info for the project you're documenting, acts as replacement for
