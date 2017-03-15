69faa5231670d7836e75f051fdab074ec3af90d7
Henning Kage <henning.kage@gmail.com>
2017-03-15 08:43:41 +0100

    Dynamic classfifiers and license

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -16,17 +16,42 @@ def read(fname):
 
 exec(open('{{ cookiecutter.project_slug }}/version.py').read())
 
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+classifiers = """
+'Development Status :: 2 - Alpha',
+'Intended Audience :: Developers',
+{%- if cookiecutter.open_source_license in license_classifiers %}
+'{{ license_classifiers[cookiecutter.open_source_license] }}',
+{%- endif %}
+'Natural Language :: English',
+'Programming Language :: Python :: 2',
+'Programming Language :: Python :: 2.7',
+'Programming Language :: Python :: 3',
+'Programming Language :: Python :: 3.2',
+'Programming Language :: Python :: 3.3',
+'Programming Language :: Python :: 3.4',
+'Programming Language :: Python :: 3.5',
+"""
+
 
 tests_require = [
     'coverage',
     'flake8',
     'pydocstyle',
     'pylint',
+    {% if cookiecutter.use_pytest == 'y' -%}
     'pytest-pep8',
     'pytest-cov',
     # for pytest-runner to work, it is important that pytest comes last in
     # this list: https://github.com/pytest-dev/pytest-runner/issues/11
-    'pytest',
+    'pytest'{%- endif %}
 ]
 
 version = '{{ cookiecutter.version }}'
@@ -38,6 +63,8 @@ setup(name='{{ cookiecutter.project_name }}',
       author='{{ cookiecutter.author_name }}',
       author_email='{{ cookiecutter.email }}',
       url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
+      classifiers=[c.strip() for c in classifiers.splitlines()
+                   if c.strip() and not c.startswith('#')],
       include_package_data=True,
       classifiers=[],
       packages=[
