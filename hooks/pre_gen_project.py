66ce1b8a6f688281d92b15a4713a88509649945c
Henning Kage <henning.kage@gmail.com>
2022-06-10 09:10:55 +0200

    Updated support Python versions

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -11,7 +11,7 @@ logger = logging.getLogger('pre_gen_project')
 
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-SUPPORTED_PYTHON_VERSIONS = ('2.7', '3.5', '3.6', '3.7', '3.8')
+SUPPORTED_PYTHON_VERSIONS = ('3.6', '3.7', '3.8', '3.9', '3.10')
 
 python_versions = [version.strip() for version in '{{ cookiecutter.python_versions }}'.split(',')]
 
